89000c11d65c232931a9e188faf6d078cffe840e
Edoardo Ruiz <edoardrui0@gmail.com>
2021-02-10 14:04:46 -0500

    added features tab, along with the 'buy now' button

--- src/Features/Features.js
+++ src/Features/Features.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import "./Features.css";
 
 class Features extends Component {
   render() {
-    return <div className="features">hello</div>;
+    return (
+      <div className="container">
+        <h2>Features</h2>
+        <h3>How do resis perform compared to dcs?</h3>
+        <span>
+          Residential proxies are slower than dcs because they are connected to
+          an ISP but, they are a lot safer and less likely to be banned.
+        </span>
+
+        <h3>How do I use them for my bot?</h3>
+        <span>
+          Resis will come in data-plans similar to those of a phone. You have a
+          limited amount of data depending on how much you buy. But this means
+          you can generate as much as you want until your data-plan runs out.
+        </span>
+
+        <button>Buy Now</button>
+      </div>
+    );
   }
 }
 
